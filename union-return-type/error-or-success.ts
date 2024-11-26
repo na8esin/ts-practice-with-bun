e8df1b3419b257ece445a82406fd0783bda84b8c
Takayuki Watanabe <watanabe@sikmi.com>
2024-11-26 17:48:58 +0900

    ファイル名など変更

--- union-return-type/error-or-success.ts
+++ union-return-type/error-or-success.ts
@@ -1,35 +1,20 @@
-type ErrorOrSuccess = {error: unknown, status: number} | {data: unknown, status: number}
+// https://qiita.com/eyuta/items/e6c992422c33a2f28a9f
 
-async function fetchUser(flag: boolean): Promise<ErrorOrSuccess> {
-    if (flag) {
-        return Promise.resolve({data: 'aaa', status: 200})
-    } else {
-        return Promise.reject({error: 'error', status: 500})
-    }
-}
+type ErrorOrSuccess =
+  | { error: unknown; status: number }
+  | { data: unknown; status: number };
 
 function getUser(flag: boolean): ErrorOrSuccess {
-    if (flag) {
-        return {data: 'aaa', status: 200}
-    } else {
-        return {error: 'error', status: 500}
-    }
+  if (flag) {
+    return { data: "aaa", status: 200 };
+  } else {
+    return { error: "error", status: 500 };
+  }
 }
 
-fetchUser(true).then((res) => {
-  // Property 'data' does not exist on type 'ErrorOrSuccess'.
-  //  Property 'data' does not exist on type '{ error: unknown; status: number; }'
-  res.data
+getUser(true).data;
+getUser(true).error;
 
-  // Property 'error' does not exist on type 'ErrorOrSuccess'.
-  //  Property 'error' does not exist on type '{ data: unknown; status: number; }'
-  res.error
-});
-
-getUser(true).data
-getUser(true).error
-
-const eOs: ErrorOrSuccess = {data: 'aaa', status: 200};
-eOs.data
-const eOs2: ErrorOrSuccess = {error: 'aaa', status: 200};
-eOs2.error
+// もちろん↓の書き方ならOK
+(getUser(true) as { data: unknown; status: number }).data;
+(getUser(true) as { error: unknown; status: number }).error;
