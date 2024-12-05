955a1da216d3e9c00a9756353050c3afbd338c4a
Takayuki Watanabe <watanabe@sikmi.com>
2024-12-05 09:07:27 +0900

    add axios-sample-another.ts

---
+++ union-return-type/axios-sample-another.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+type User = {
+  id: number;
+  name: string;
+};
+
+type FetchUserResponse = {
+  status?: number;
+  error?: unknown | null;
+  data?: User;
+};
+
+async function fetchUser(): Promise<FetchUserResponse | null> {
+  try {
+    const { data, status } = await axios.get<User>("/user?ID=12345");
+    return { data, status };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return { status: error?.status, error: error };
+    } else {
+      throw error;
+    }
+  }
+}
+
+fetchUser().then((res) => {
+  // 全部エラーにならない
+  res?.data;
+  res?.error;
+  res?.status;
+});
