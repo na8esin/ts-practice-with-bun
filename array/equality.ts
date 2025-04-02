33717d920b77b5300ee65288e2b045fe06d33c05
Takayuki Watanabe <watanabe@sikmi.com>
2025-04-02 18:21:21 +0900

    空配列同士をtoString()で比較するとtrueになる

--- array/equality.ts
+++ array/equality.ts
@@ -3,4 +3,6 @@ console.log(["a"] === ["a"]); // false
 console.log(["a"].toString() === ["a"].toString()); // true
 console.log(["a", "b"].toString() === ["b", "a"].toString()); // false
 
+console.log([].toString() === [].toString()); // true
+
 // 重複、並び順が考慮できればねぇ。
